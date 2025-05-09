e9981f055ec92ffe61158e4a93ad05e49e17dc90
Amaan Mujawar <amaanmujawar175@gmail.com>
2025-05-09 23:49:54 +0530

    Create ascii_and_character_type.c

---
+++ ascii_and_character_type.c
@@ -0,0 +1,34 @@
+/*
+Problem Statement: Write a C program to input a character, print its ASCII value, and classify it as vowel, capital letter, 
+small letter, digit, or special character.
+*/
+
+#include<stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    printf("The ASCII value of %c is %d.\n", ch, ch);
+
+    if ((ch == 'A') || (ch == 'E') || (ch == 'I') || (ch == 'O') || (ch == 'U')) {
+        printf("\nIts a Capital Vowel.\n");
+    }
+    else if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u')) {
+        printf("\nIts a Small Vowel.\n");
+    }
+    else if ((ch >= 65) || (ch <= 90)) {
+        printf("\nIts a Capital Character.\n");
+    }
+    else if ((ch >= 97) || (ch <= 122)) {
+        printf("\nIts a Small Character.\n");
+    }
+    else if ((ch >= 48) || (ch == 57)) {
+        printf("\nIts a Number.\n");
+    }
+    else {
+        printf("\nIts Other Character.\n");
+    }
+}
